12f611ca85a4fa70a8262e48b6bd5cb6c3a85a02
Weretik <vitaliythupin@gmail.com>
2025-05-02 21:48:45 +0300

    feat(logging): Add Serilog config and global exception middleware

--- Application/Common/Settings/ImageSettings.cs
+++ Application/Common/Settings/ImageSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Common.Settings
+﻿namespace Application.Common.Settings
 {
     public class ImageSettings
     {
---
+++ Web/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Serilog.Context;
+
+namespace Web.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var user = context.User.Identity?.IsAuthenticated == true
+                ? context.User.Identity.Name
+                : "Anonymous";
+
+            var ip = context.Connection.RemoteIpAddress?.ToString();
+            var requestId = Guid.NewGuid(); // Можно заменить на real CorrelationId, если появится
+
+            using (LogContext.PushProperty("User", user))
+            using (LogContext.PushProperty("ClientIP", ip))
+            using (LogContext.PushProperty("RequestPath", context.Request.Path))
+            using (LogContext.PushProperty("RequestId", requestId))
+                try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                
+
+                _logger.LogError(ex, "Unhandled exception occurred while processing request.");
+
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("An unexpected error occurred.");
+            }
+        }
+    }
+}
