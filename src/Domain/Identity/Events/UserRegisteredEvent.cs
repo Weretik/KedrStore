dd8bf374b01632fed60c15de02bb636ba9d0c8bf
Weretik <vitaliythupin@gmail.com>
2025-06-04 23:38:26 +0300

    feat(domain): add Identity core elements (AppUserId, IAppUserRepository, UserRegisteredEvent)

---
+++ src/Domain/Identity/Events/UserRegisteredEvent.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Events;
+using Domain.Identity.Entities;
+using Domain.Identity.ValueObjects;
+using Domain.Events;
+using Domain.Identity.Entities;
+using Domain.Identity.ValueObjects;
+
+namespace Domain.Identity.Events
+{
+    /// <summary>
+    /// Событие, представляющее регистрацию нового пользователя в системе
+    /// </summary>
+    public class UserRegisteredEvent : BaseDomainEvent
+    {
+        public AppUserId UserId { get; }
+        public string FullName { get; }
+        public string Email { get; }
+
+        public UserRegisteredEvent(AppUser user)
+        {
+            UserId = user.Id;
+            FullName = user.FullName;
+            Email = user.Email;
+
+        }
+    }
+}
---
+++ src/Domain/Identity/ValueObjects/AppUserId.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common;
+
+namespace Domain.Identity.ValueObjects
+{
+    public class AppUserId : EntityId
+    {
+        public AppUserId(int value) : base(value) { }
+    }
+}
