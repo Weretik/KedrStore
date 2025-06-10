79b174d0cf6767c1890adfdcc94270fb6df3f5b7
Weretik <vitaliythupin@gmail.com>
2025-06-10 19:22:59 +0300

    feat(infrastructure): add CatalogDbContext with mappings for catalog entities
    - Added CatalogDbContext
    - Registered catalog entities for EF Core

---
+++ src/Infrastructure/Catalog/CatalogDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Domain.Catalog.Entities;
+
+namespace Infrastructure.Catalog;
+
+public class CatalogDbContext : DbContext
+{
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+        : base(options) { }
+
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogDbContext).Assembly);
+    }
+}
