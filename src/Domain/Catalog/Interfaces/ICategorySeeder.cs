66e31651d5c79532b1f9b9b07fa879b6722151c9
Weretik <vitaliythupin@gmail.com>
2025-06-12 19:17:18 +0300

    feat(domain): add ICategorySeeder and IProductSeeder interfaces

---
+++ src/Domain/Catalog/Interfaces/ICategorySeeder.cs
@@ -0,0 +1,6 @@
+namespace Domain.Catalog.Interfaces;
+
+public interface ICategorySeeder
+{
+    Task SeedAsync();
+}
---
+++ src/Domain/Catalog/Interfaces/IProductSeeder.cs
@@ -0,0 +1,6 @@
+namespace Domain.Catalog.Interfaces;
+
+public interface IProductSeeder
+{
+    Task SeedAsync();
+}
