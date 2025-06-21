2b966ca2f474ed4bf618399a73915a35bdfb8190
Weretik <vitaliythupin@gmail.com>
2025-06-21 14:45:45 +0300

    feat(application): add background job abstraction and options
    - Introduced IBackgroundJobService interface for scheduling and executing background tasks
    - Added IJobHandler for typed background job handling
    - Defined JobOptions DTO to configure job metadata and constraints
    - Updated global usings for background job infrastructure

--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -10,25 +10,26 @@ global using System.Linq.Expressions;
 
 // Microsoft namespaces
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Infrastructure;
-global using Microsoft.Extensions.Logging;
 
 // Application namespaces
 global using Application.Common.Abstractions.Events;
-global using Application.Common.Behaviors;
-global using Application.Common.Results;
 global using Application.Common.Abstractions.UseCase;
 global using Application.Common.Abstractions.Pagination;
-global using Application.Common.Pagination;
 global using Application.Common.Abstractions.Mapping;
-global using Application.Common.Events;
 global using Application.Common.Abstractions.Result;
-global using Application.Interfaces;
-global using Application.Catalog.DTOs;
+global using Application.Common.Abstractions.Background;
+global using Application.Common.Behaviors;
+global using Application.Common.Results;
+global using Application.Common.Pagination;
+global using Application.Common.Events;
 global using Application.Common.Exceptions;
 global using Application.Common.Errors;
+global using Application.Interfaces;
+global using Application.Catalog.DTOs;
 global using Application.DTOs;
 
 // Domain namespaces
