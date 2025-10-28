baa7b2c972aa53cefda0a6eca9797dcdf35aa66f
Vitaliy <VitaliyThupin@gmail.com>
2025-10-28 13:24:33 +0200

    fix(catalog): correct base type in ProductType enumeration

--- src/Domain/Catalog/Enumerations/ProductType.cs
+++ src/Domain/Catalog/Enumerations/ProductType.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Catalog.Enumerations;
 
-public sealed class ProductType(string name, int value) : SmartEnum<PriceType>(name, value)
+public sealed class ProductType(string name, int value) : SmartEnum<ProductType>(name, value)
 {
     public static readonly ProductType Doors = new("Двері", 1);
     public static readonly ProductType DoorHardware = new("Дверна фурнітура", 2);
