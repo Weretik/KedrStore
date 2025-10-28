8ac8193eb01ebbb292cf736dc582ac1c29df9c1b
Vitaliy <VitaliyThupin@gmail.com>
2025-10-28 13:29:14 +0200

    chore(catalog): add `productType` property to `CatalogFromXml` record to support product-type distinction during catalog import

--- src/Application/Catalog/Commands/ImportCatalogFromXml/CatalogFromXml.cs
+++ src/Application/Catalog/Commands/ImportCatalogFromXml/CatalogFromXml.cs
@@ -1,4 +1,4 @@
 ﻿namespace Application.Catalog.Commands.ImportCatalogFromXml;
 
-public sealed  record CatalogFromXml(string FileName, string? ContentType, long FileSize, Stream Content)
+public sealed  record CatalogFromXml(string FileName, string? ContentType, long FileSize, Stream Content, int productType)
     : ICatalogFromXml;
