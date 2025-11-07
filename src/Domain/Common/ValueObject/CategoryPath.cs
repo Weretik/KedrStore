c3f23e6255b099cba501d37c26bef664ae235e8c
Vitaliy <VitaliyThupin@gmail.com>
2025-11-07 19:25:53 +0200

    feat(domain): add CategoryPath value object
    - Introduced `CategoryPath` to encapsulate category path logic
    - Ensures validation for path format and improves type safety
    - Provides methods for path manipulation, enhancing reusability and consistency

---
+++ src/Domain/Common/ValueObject/CategoryPath.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Common.ValueObject;
+
+[ValueObject<string>(conversions: Conversions.None)]
+public readonly partial struct CategoryPath
+{
+    public static readonly CategoryPath None = From("n0");
+
+    private static readonly Regex _ltreeRegex = MyRegex();
+
+    private static Validation Validate(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Validation.Invalid("Path is required.");
+
+        if (!_ltreeRegex.IsMatch(value))
+            return Validation.Invalid("Invalid ltree format.");
+
+        return Validation.Ok;
+    }
+
+    public static CategoryPath Root(string node) => From($"n{node}");
+    public CategoryPath Append(string node) => From($"{Value}.n{node}");
+
+    [GeneratedRegex(@"^[A-Za-z0-9_]+(\.[A-Za-z0-9_]+)*$", RegexOptions.Compiled)]
+    private static partial Regex MyRegex();
+}
