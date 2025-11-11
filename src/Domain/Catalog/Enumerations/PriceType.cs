5aeaf48ed6dd0b806ab6e5c33fc18e02a69be480
Vitaliy <VitaliyThupin@gmail.com>
2025-11-11 14:29:56 +0200

    feat(domain): add new price types for Kyiv retail
    - Introduced `KyivRetailUsd` and `KyivRetailUah` price types
    - Supports specific pricing needs for Kyiv retail in USD and UAH

--- src/Domain/Catalog/Enumerations/PriceType.cs
+++ src/Domain/Catalog/Enumerations/PriceType.cs
@@ -16,5 +16,8 @@ public sealed class PriceType(string name, int value, string title) : SmartEnum<
     public static readonly PriceType SmallWholesaleGrn = new("price_9", 9,"Дрібний опт грн");
     public static readonly PriceType Retail = new("price_10", 10,"Роздріб");
     public static readonly PriceType Retail30Percent= new("price_12", 12,"Роздріб (халтмаг) 30%");
+    public static readonly PriceType KyivRetailUsd   = new("price_13", 13, "Київ роздріб usd");
+    public static readonly PriceType KyivRetailUah    = new("price_14", 14, "Київ роздріб грн");
+
 }
 
