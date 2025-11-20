4fb29566cfa19704d60bf2e88589c989b203c0c5
Vitaliy <VitaliyThupin@gmail.com>
2025-11-20 17:18:10 +0200

    feat(catalog): add ProductType to ProductCategory entity
    - Introduced `ProductType` property to the `ProductCategory` entity
    - Updated constructors and factory methods to enforce `ProductType`
    - Ensures stricter typing and validation for product categorization

--- src/Domain/Catalog/Entities/ProductCategory.cs
+++ src/Domain/Catalog/Entities/ProductCategory.cs
@@ -1,27 +1,36 @@
-﻿using Domain.Catalog.ValueObjects;
+﻿using Domain.Catalog.Enumerations;
+using Domain.Catalog.ValueObjects;
 using Domain.Common.ValueObject;
 
 namespace Domain.Catalog.Entities;
 
 public class ProductCategory : BaseCategory<ProductCategoryId, ProductCategory>, IAggregateRoot
 {
+    #region Properties
+    public ProductType ProductType { get; private set; }
+    #endregion
+
     #region Constructors
     private ProductCategory() { }
-    private ProductCategory(ProductCategoryId id, string name, CategoryPath path)
+    private ProductCategory(ProductCategoryId id, string name, CategoryPath path, ProductType productType)
     {
         SetCategoryId(id);
         SetName(name);
         SetPath(path);
+        SetProductType(productType);
     }
     #endregion
 
     #region Factories
-    public static ProductCategory Create(ProductCategoryId id, string name, CategoryPath path)
-        => new(id, name, path);
-    public static ProductCategory CreateRoot(ProductCategoryId id, string name)
-        => new(id, name, CategoryPath.Root(id.ToString()));
-    public static ProductCategory CreateChild(ProductCategoryId id, string name, ProductCategory parent)
-        => new(id, name, parent.Path.Append(id.ToString()));
+    public static ProductCategory Create(ProductCategoryId id, string name, CategoryPath path, ProductType productType)
+        => new(id, name, path, productType);
+    public static ProductCategory CreateRoot(ProductCategoryId id, string name, ProductType productType)
+        => new(id, name, CategoryPath.Root(id.ToString()), productType);
+    public static ProductCategory CreateChild(ProductCategoryId id, string name, ProductCategory parent, ProductType productType)
+        => new(id, name, parent.Path.Append(id.ToString()), productType);
     #endregion
 
+    #region Validation & Setters
+    private void SetProductType(ProductType productType) => ProductType = Guard.Against.Default(productType, nameof(productType));
+    #endregion
 }
