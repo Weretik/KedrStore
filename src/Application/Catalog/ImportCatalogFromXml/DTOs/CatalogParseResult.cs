5853f4d6a3348b4058120552509b0f2632e51825
Vitaliy <VitaliyThupin@gmail.com>
2025-12-08 08:38:55 +0200

    feat(catalog): remove CatalogParseResult DTO
    - Removed `CatalogParseResult` as it is no longer required
    - Simplifies the codebase by removing unused components

--- src/Application/Catalog/ImportCatalogFromXml/DTOs/CatalogParseResult.cs
+++
@@ -1,5 +0,0 @@
-﻿using Application.Catalog.Shared;
-
-namespace Application.Catalog.ImportCatalogFromXml;
-
-public sealed record CatalogParseResult(IReadOnlyList<ImportCategoryDto> Categories, IReadOnlyList<ProductDto> Products);
