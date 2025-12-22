ee1de7ef5cfc917b087e6754ebc04767eadfd131
Vitaliy <VitaliyThupin@gmail.com>
2025-12-22 16:46:59 +0200

    feat(integration): add OneC SOAP client factory
    - Introduced a factory class for creating OneC SOAP clients
    - Ensures proper configuration and authentication setup
    - Supports improved integration with OneC service

---
+++ src/Infrastructure/Common/Integrations/OneC/Factory/OneCSoapClientFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.ServiceModel;
+using Infrastructure.Common.Integrations.OneC.Generated;
+
+namespace Infrastructure.Common.Integrations.OneC;
+
+public sealed class OneCSoapClientFactory(IConfiguration configuration)
+{
+    public Kontra1PortTypeClient Create()
+    {
+        var endpoint = configuration["OneCSoap:Endpoint"]
+                       ?? throw new InvalidOperationException("OneCSoap:Endpoint is missing");
+        var username = configuration["OneCSoap:Username"]
+                       ?? throw new InvalidOperationException("OneCSoap:Username is missing");
+        var password = configuration["OneCSoap:Password"]
+                       ?? throw new InvalidOperationException("OneCSoap:Password is missing");
+
+
+        var binding = new BasicHttpBinding(BasicHttpSecurityMode.None)
+        {
+            MaxReceivedMessageSize = int.MaxValue,
+            AllowCookies = true
+        };
+
+        var address = new EndpointAddress(endpoint);
+
+        var client = new Kontra1PortTypeClient(binding, address);
+
+        client.Endpoint.EndpointBehaviors.Add(new BasicAuthEndpointBehavior(username, password));
+
+        return client;
+    }
+}
