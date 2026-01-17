e61a288ac94c77128c99cb35758dde83dcc4a834
Vitaliy <VitaliyThupin@gmail.com>
2026-01-17 19:50:11 +0200

    refactor(catalog): rename ProductBySlugQuery for consistency
    - Updated `ProductBySlugQuery` to `GetProductBySlugQuery` to align with naming conventions used across query implementations.
    - Improves clarity and adherence to established patterns, enhancing maintainability.

--- src/Catalog/Catalog.Application/Features/Products/GetById/GetProductByIdQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using Catalog.Application.Features.Products.GetById.DTOs;
-using Catalog.Application.Integrations.OneC.DTOs;
-
-namespace Catalog.Application.Features.Products.GetById;
-
-public sealed record ProductBySlugQuery(GetProductBySlugRequest Request) : IQuery<Result<ProductBySlugDto>>;
---
+++ src/Catalog/Catalog.Application/Features/Products/GetById/GetProductBySlugQuery.cs
@@ -0,0 +1,5 @@
+﻿using Catalog.Application.Features.Products.GetById.DTOs;
+
+namespace Catalog.Application.Features.Products.GetById;
+
+public sealed record GetProductBySlugQuery(GetProductBySlugRequest Request) : IQuery<Result<ProductBySlugDto>>;
