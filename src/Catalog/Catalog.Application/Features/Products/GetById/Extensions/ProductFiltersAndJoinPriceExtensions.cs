81a24751e61ea36674d1fa444f5d10935333d4a1
Vitaliy <VitaliyThupin@gmail.com>
2026-01-17 19:49:48 +0200

    feat(catalog): implement query handler for GetProductBySlug with extensions
    - Added `GetProductBySlugQueryHandler` to replace the previous `ProductBySlugQueryHandler` for enhanced functionality and alignment with modern patterns.
    - Introduced extension methods for product filtering and joining prices and categories.
    - Improved code maintainability, readability, and support for advanced query composition.

---
+++ src/Catalog/Catalog.Application/Features/Products/GetById/Extensions/ProductFiltersAndJoinPriceExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Catalog.Application.Features.Products.GetById.DTOs;
+using Catalog.Domain.ValueObjects;
+
+namespace Catalog.Application.Features.Products.GetById.Extensions;
+
+public static class ProductFiltersAndJoinPriceExtensions
+{
+    public static IQueryable<ProductBySlugDto> JoinPricesAndCategoryForProductBySlug(
+        this IQueryable<Product> productsQuery,
+        IQueryable<ProductPrice> pricesQuery,
+        IQueryable<ProductCategory> categoriesQuery,
+        GetProductBySlugRequest request)
+    {
+        productsQuery = productsQuery.Where(p => p.ProductSlug == request.Slug);
+
+        var withCategory = productsQuery.LeftJoin(
+            categoriesQuery,
+            p => p.CategoryId,
+            c => c.Id,
+            (p, c) => new { p, c }
+        );
+
+        var priceTypeId = PriceTypeId.From(request.PriceTypeId);
+        var filteredPrices  = pricesQuery.Where(pr => pr.PriceTypeId == priceTypeId);
+
+        var withCategoryAndPrice = withCategory.LeftJoin(
+            filteredPrices,
+            x => x.p.Id,
+            pr => pr.ProductId,
+            (x, pr) => new { x.p, x.c, pr }
+        );
+        return withCategoryAndPrice.Select(x => new ProductBySlugDto
+        {
+            Id = x.p.Id.Value,
+            Name = x.p.Name,
+            Photo = x.p.Photo,
+            Scheme = x.p.Sсheme,
+            Stock = x.p.Stock,
+            CategoryName = x.c.Name,
+            CategorySlug = x.c.Slug,
+            Price = x.pr!.Amount
+        });
+    }
+
+}
---
+++ src/Catalog/Catalog.Application/Features/Products/GetById/GetProductBySlugQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Application.Features.Products.GetById.DTOs;
+using Catalog.Application.Features.Products.GetById.Extensions;
+using Catalog.Application.Persistence;
+
+namespace Catalog.Application.Features.Products.GetById;
+
+public class GetProductBySlugQueryHandler(IReadCatalogDbContext catalogDbContext)
+    : IQueryHandler<GetProductBySlugQuery, Result<ProductBySlugDto>>
+{
+    public async ValueTask<Result<ProductBySlugDto>> Handle(GetProductBySlugQuery query, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(query);
+
+        var request = query.Request;
+
+        var queryResult = catalogDbContext.Products
+            .AsNoTracking()
+            .JoinPricesAndCategoryForProductBySlug(
+                catalogDbContext.ProductPrices.AsNoTracking(),
+                catalogDbContext.Categories.AsNoTracking(),
+                request);
+
+        var result = await queryResult.FirstOrDefaultAsync(cancellationToken);
+
+        if (result is null)
+            return Result.NotFound();
+
+        return Result.Success(result);
+    }
+}
