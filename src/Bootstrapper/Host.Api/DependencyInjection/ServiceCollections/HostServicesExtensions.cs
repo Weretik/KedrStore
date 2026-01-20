1bf39a439e8d2258f5f6424ad65d7225f965f648
Vitaliy <VitaliyThupin@gmail.com>
2026-01-20 19:34:09 +0200

    feat(bootstrapper): add CORS service to host services
    - Included `AddCorsService` extension for streamlined CORS configuration.
    - Enhances support for cross-origin requests in API deployments.

--- src/Bootstrapper/Host.Api/DependencyInjection/ServiceCollections/HostServicesExtensions.cs
+++ src/Bootstrapper/Host.Api/DependencyInjection/ServiceCollections/HostServicesExtensions.cs
@@ -21,6 +21,7 @@ public static class HostServicesExtensions
         services.AddCatalogApplicationServices(configuration);
 
         services.AddHangfire(configuration);
+        services.AddCorsService();
 
         services.AddMediatorPipeline();
         services.AddFluentValidation();
