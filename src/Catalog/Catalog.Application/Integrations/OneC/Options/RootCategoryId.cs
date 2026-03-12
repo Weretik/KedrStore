69ac21f6f076f6951fb0fcda331354f1ef9e99ac
Vitaliy <VitaliyThupin@gmail.com>
2026-03-12 18:38:51 +0200

    feat(onec): add manual category groups for hardware and doors
    - Introduced `HardwareManualCategoryGroups` and `DoorsManualCategoryGroups` configurations in app settings.
    - Updated `RootCategoryId` to include manual category groups for enhanced categorization.
    - Supports better organization and custom grouping of categories in OneC integration.

--- src/Catalog/Catalog.Application/Integrations/OneC/Options/RootCategoryId.cs
+++ src/Catalog/Catalog.Application/Integrations/OneC/Options/RootCategoryId.cs
@@ -4,4 +4,6 @@ public sealed class RootCategoryId
 {
     public required string DoorsRootCategoryId { get; init; }
     public required string HardwareRootCategoryId { get; init; }
+    public List<ManualCategoryGroupOption> HardwareManualCategoryGroups { get; init; } = [];
+    public List<ManualCategoryGroupOption> DoorsManualCategoryGroups { get; init; } = [];
 }
